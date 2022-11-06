500b5f37ca4571b8190fd6280b5a001df871c6f4
Prashant Solanki <solankisrp2@gmail.com>
2022-11-06 06:10:09 +0000

    Updates Flame and Cloudflared
    Add Flame to Essentials stack
    Switched to container for cloudflared

--- scripts/docker-service-run.sh
+++ scripts/docker-service-run.sh
@@ -7,12 +7,14 @@ set +o allexport
 docker network create -d overlay --attachable intranet
 docker network create -d overlay --attachable db
 
-docker service create --replicas 1 \
-    --name tunnel \
-    --hostname tunnel \
-    --network intranet \
-    cloudflare/cloudflared:latest \
-        --no-autoupdate run \
-        --token $cloudflared_token
+docker run cloudflare/cloudflared:latest tunnel --no-autoupdate run --token $cloudflared_token
+
+# docker service create --replicas 3 \
+#     --name tunnel \
+#     --hostname tunnel \
+#     --network intranet \
+#     cloudflare/cloudflared:latest \
+#         --no-autoupdate run \
+#         --token $cloudflared_token
 cd /tmp
-docker stack deploy --compose-file <(docker-compose config) portainer
+docker stack deploy --compose-file <(docker-compose config) essentials
